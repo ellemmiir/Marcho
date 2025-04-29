88a1d46e0aa3906e5adbecb1e764bae05738cc39
Metsuki Sutemi <metsuki-sutemi@mail.ru>
2025-04-29 10:26:31 +0300

    added filters at page "shop"

--- docs/js/main.js
+++ docs/js/main.js
@@ -13757,6 +13757,7 @@ return jQuery;
 
 $(function () {
 
+
   $('.menu__btn').on('click', function () {
     $('.menu__list').toggleClass('menu__list--active');
     $('.menu__btn').toggleClass('menu__btn--active');
